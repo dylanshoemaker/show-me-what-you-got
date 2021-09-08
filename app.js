28c236b28a982371c4df0cdee0a32f3233aef383
Dylan Shoemaker <me@dylanshoemaker.com>
2021-09-08 11:24:59 -0700

    build initial prompt

--- app.js
+++ app.js
@@ -1,27 +1,77 @@
-const inquire = require('inquirer');
+const inquire = require("inquirer");
 require("dotenv").config();
 const mysql = require("mysql2");
-const cTable = require('console.table');
-const PORT = process.env.PORT;
+const cTable = require("console.table");
+const inquirer = require("inquirer");
 
 // Connect to database
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    // Your MySQL username,
-    user: process.env.DB_USER,
-    // Your MySQL password
-    password: process.env.DB_PASS,
-    database: "tracker",
-  }
-);
-
-
-
+const db = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  // Your MySQL username,
+  user: process.env.DB_USER,
+  // Your MySQL password
+  password: process.env.DB_PASS,
+  database: "tracker",
+});
 
 // Start server after DB connection
-db.connect(err => {
+db.connect((err) => {
   if (err) throw err;
   console.log("Connected to the Employee Tracker.");
-  //start the app here
+  startPrompt(); //start the prompt
 });
+
+function startPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "initialPrompt",
+        message: "What would you like to do?",
+        choices: [
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add A Department",
+          "Add A Role",
+          "Add An Employee",
+          "Update An Employee Role",
+        ],
+      },
+    ])
+    .then(function (val) {
+      // this is to turn these choices into functions
+      switch (val.initialPrompt) {
+        case "View All Departments":
+          viewAllDeps();
+          break;
+
+        case "View All Roles":
+          viewAllRoles();
+          break;
+        case "View All Employees":
+          viewAllEmps();
+          break;
+
+        case "Add A Department":
+          addDeps();
+          break;
+
+        case "Add A Role":
+          addRole();
+          break;
+
+        case "Add An Employee":
+          addEmp();
+          break;
+
+        case "Update An Employee Role":
+          updateEmpRole();
+          break;
+      }
+    });
+}
+function viewAllDeps(){
+  console.log("this works")
+}
